c3b73ced336445a082aa4ff2bb904e23ef65d19d
Ioan Cîrjă <ioan.cirja@student.tuiasi.ro>
2023-07-21 16:32:14 +0300

    Add/Complete/Remove without internal storage

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-const OpenDialogBtn = document.getElementById("OpenDialogBtn");
-const CancelDialogBtn = document.getElementById("CancelDialogBtn");
+const openDialogBtn = document.getElementById("openDialogBtn");
+const cancelDialogBtn = document.getElementById("cancelDialogBtn");
 
-const TaskDialog = document.getElementById("TaskDialog");
-const TaskForm = document.getElementById("TaskForm");
-const CompletedTaskList = document.getElementById("CompletedTaskList");
-const UncompletedTaskList= document.getElementById("UncompletedTaskList");
+const taskDialog = document.getElementById("taskDialog");
+const taskForm = document.getElementById("taskForm");
+const completedTaskList = document.getElementById("completedTaskList");
+const uncompletedTaskList = document.getElementById("uncompletedTaskList");
 
-OpenDialogBtn.addEventListener("click", () =>
-{
-    TaskDialog.showModal();
+openDialogBtn.addEventListener("click", () => {
+    taskDialog.showModal();
 });
-CancelDialogBtn.addEventListener("click", () =>
-{
-    TaskDialog.close();
 
+cancelDialogBtn.addEventListener("click", () => {
+    taskDialog.close();
 });
+
+function addTask(title, description, assignee) {
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>Assignee: ${assignee}</p>
+        <button id="completeBtn">Mark as Completed</button>
+        <button id="deleteBtn">Remove</button>
+    `;
+
+    const completeBtn = taskItem.querySelector("#completeBtn");
+    completeBtn.addEventListener("click", () => {
+        taskItem.classList.toggle("completed");
+        completedTaskList.appendChild(taskItem);
+    });
+
+    const deleteBtn = taskItem.querySelector("#deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+        taskItem.remove();
+    });
+
+    uncompletedTaskList.appendChild(taskItem);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const title = taskForm.title.value.trim();
+    const description = taskForm.description.value.trim();
+    const assignee = taskForm.asignee.value.trim();
+
+    addTask(title, description, assignee);
+    taskForm.reset();
+
+    taskDialog.close();
+
+}
+
+taskForm.addEventListener("submit", handleFormSubmit);
